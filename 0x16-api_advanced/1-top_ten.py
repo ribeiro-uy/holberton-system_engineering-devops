8c305c8002282689e62821cc1eadf8fbd4752b0b
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-18 17:57:21 -0300

    change top to hot

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     of the first 10 hot posts listed for a given subreddit."""
 
     headers = {'User-Agent': 'SoyYo!'}
-    url = "https://www.reddit.com/r/" + subreddit + "/top.json?limit=10"
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=10"
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         for element in res.json().get('data').get('children'):
