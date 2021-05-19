ea81c3aff4b4a482494fa1c4c91a56a02af09c54
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-19 15:35:35 -0300

    change path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix path name in the file wp-settings.php
 exec { 'fix_line':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+  path    => ['/bin']
 }
